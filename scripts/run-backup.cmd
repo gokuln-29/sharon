7323aafcf7c489d708820042300247e44f29c454
Gokulnath <coolz@Gokulnaths-MacBook-Air.local>
2026-06-18 15:11:46 +0530

    Update changes

--- scripts/run-backup.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Wrapper used by Windows Task Scheduler. Don't rename without updating the task.
-cd /d "%~dp0\.."
-"C:\Program Files\nodejs\node.exe" "scripts\backup-db.mjs" >> "D:\SharonBackups\backup.log" 2>&1
